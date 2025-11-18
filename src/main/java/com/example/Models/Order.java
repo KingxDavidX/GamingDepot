5d9df78f933eaea25157fe9452b380ca9dbd2780
snutt10 <snutt@student.neumont.edu>
2025-11-17 19:19:37 -0700

    Update to Order class and create 2 new classes
    Add getters and setters to add and create OrderDataArray and OrderController

--- src/main/java/com/example/Models/Order.java
+++ src/main/java/com/example/Models/Order.java
@@ -6,5 +6,56 @@ public class Order {
     int customerId;
     int orderId;
     Date orderDate;
-    
+    String status;
+    float totalAmount;
+
+    public Order(int customerId, int orderId, Date orderDate, String status, float totalAmount) {
+        this.customerId = customerId;
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.status = status;
+        this.totalAmount = totalAmount;
+    }
+
+    public Order() {}
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public float getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(float totalAmount) {
+        this.totalAmount = totalAmount;
+    }
 }
