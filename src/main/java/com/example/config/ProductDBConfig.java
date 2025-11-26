ab5f28da79f1b99894383b7e108ad705bf8552db
snutt10 <snutt@student.neumont.edu>
2025-11-25 19:52:49 -0700

    Adding configs and changes to application.properties and compose.yaml
    I added the config fill as well the neccessary work inside, and changed application.properties and compose.yaml to have the info for the second database

---
+++ src/main/java/com/example/config/ProductDBConfig.java
@@ -0,0 +1,69 @@
+package com.example.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "firstEntityManagerFactoryBean",
+        basePackages = {"com.example.Repositories"},
+        transactionManagerRef = "firstTransactionManager"
+)
+public class ProductDBConfig {
+    @Autowired
+    private Environment env;
+
+    @Bean(name = "productDataSource")
+    @Primary
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+
+        return dataSource;
+    }
+
+    @Primary
+    @Bean(name = "firstEntityManagerFactoryBean")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(){
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setDataSource(dataSource());
+        bean.setPackagesToScan("com.example.model");
+
+        JpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        bean.setJpaVendorAdapter(adapter);
+
+        Map<String,String> props = new HashMap<>();
+        props.put("hibernate.dialect","org.hibernate.dialect.MySQLDialect");
+        props.put("hibernate.show_sql","true");
+        props.put("hibernate.hbm2ddl.auto","update");
+        bean.setJpaPropertyMap(props);
+
+        return bean;
+    }
+
+    @Bean(name = "firstTransactionManager")
+    @Primary
+    public PlatformTransactionManager transactionManager(){
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
+        return manager;
+    }
+}
