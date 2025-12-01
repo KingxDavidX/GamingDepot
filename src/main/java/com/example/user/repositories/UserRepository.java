6b25101032ff6a34d6cc3a1648b72911d3833b5c
KingxDavidX <davianh2016@gmail.com>
2025-11-30 22:59:25 -0700

    Refactor: changed project structure to be the right case and also changed names to be easier to read and seperated repository files

--- src/main/java/com/example/user/repositories/UserRepository.java
+++ src/main/java/com/example/user/repositories/UserRepository.java
@@ -2,5 +2,5 @@ package com.example.user.repositories;
 import com.example.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserReposoitory extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
