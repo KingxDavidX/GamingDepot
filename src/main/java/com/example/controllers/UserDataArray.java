44f9538284f0e8685ccf0c7c3efc72e9a6790210
KingxDavidX <davianh2016@gmail.com>
2025-12-01 19:07:23 -0700

    Refactor: added more field to add login and registration

--- src/main/java/com/example/controllers/UserDataArray.java
+++ src/main/java/com/example/controllers/UserDataArray.java
@@ -8,7 +8,7 @@ public class UserDataArray {
     private static final ArrayList<User> users = new ArrayList<>();
 
     public static String add(User user) {
-        if (users.stream().filter(i -> i.getUserId() == user.getUserId()).findFirst().orElse(null) == null) {
+        if (users.stream().filter(i -> i.getUser_id() == user.getUser_id()).findFirst().orElse(null) == null) {
             users.add(user);
             return "User Added";
         } else {
@@ -21,11 +21,11 @@ public class UserDataArray {
     }
 
     public static User getById(int id) {
-        return users.stream().filter(user-> user.getUserId() == id).findFirst().orElse(null);
+        return users.stream().filter(user-> user.getUser_id() == id).findFirst().orElse(null);
     }
 
     public static User update(User user) {
-        User temp_users= UserDataArray.users.stream().filter(i -> i.getUserId() == user.getUserId()).findFirst().orElse(null);
+        User temp_users= UserDataArray.users.stream().filter(i -> i.getUser_id() == user.getUser_id()).findFirst().orElse(null);
         if (user != null) {
             users.remove(user);
             users.add(user);
@@ -35,7 +35,7 @@ public class UserDataArray {
     }
 
     public static String deleteById(int id) {
-        return users.removeIf(user-> user.getUserId() == id) ? "Removed" : "Not Found";
+        return users.removeIf(user-> user.getUser_id() == id) ? "Removed" : "Not Found";
     }
 
 
--- src/main/java/com/example/transactions/model/User.java
+++ src/main/java/com/example/transactions/model/User.java
@@ -1,33 +1,35 @@
 package com.example.transactions.model;
 
+import java.util.Date;
+
 public class User {
 
     private long user_id;
-    private String name;
     private String email;
+    private String password_hash;
+    private String first_name;
+    private String last_name;
+    private Date Created_at;
 
     public User() {}
 
-    public User(long user_id, String name, String email) {
+    public User(long user_id, String email, String password_hash, String first_name, String last_name, Date created_at) {
         this.user_id = user_id;
-        this.name = name;
         this.email = email;
+        this.password_hash = password_hash;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.Created_at = created_at;
     }
+    
+    //region Getters/Setters
 
-    public long getUserId() {
+    public long getUser_id() {
         return user_id;
     }
 
-    public void setUserId(long id) {
+    public void setUser_id(long id) {
         this.user_id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getEmail() {
@@ -37,6 +39,40 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getPassword_hash() {
+        return password_hash;
+    }
+
+    public void setPassword_hash(String password_hash) {
+        this.password_hash = password_hash;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Date getCreated_at() {
+        return Created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        Created_at = created_at;
+    }
+
+    //endregion
 
     public void login() {
         //TODO: Implement funtionality
