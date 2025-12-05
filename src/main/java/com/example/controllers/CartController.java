454ce08b83001c39237e64c17506e4a8b0cdd20a
snutt10 <snutt@student.neumont.edu>
2025-12-04 19:23:05 -0700

    These are changes that I made before I pulled

--- src/main/java/com/example/controllers/CartController.java
+++ src/main/java/com/example/controllers/CartController.java
@@ -1,13 +1,20 @@
 package com.example.controllers;
 
+import com.example.transactions.dao.CartDao;
 import com.example.transactions.model.Cart;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("cart")
+@RequestMapping("/cart")
 public class CartController {
-    public CartController() {}
+    private final CartDao cartDao;
+
+    public CartController(CartDao cartDao) {
+        this.cartDao = cartDao;
+    }
+
+    //Methods: getProductID, addProductID, deleteProductID, getTotalPrice, addTotalPrice, removeTotalPrice,
 
 
 }
