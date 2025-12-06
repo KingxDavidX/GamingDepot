e75953a9f9c71bbc28cb5d6557607b960cf6df2f
snutt10 <snutt@student.neumont.edu>
2025-12-05 18:45:06 -0700

    Cart Dao methods
    added a methods that adds a cart (my thought is that this will be called when a user is added but I have no personal preference how its used) and a method that updates a cart (this will hopefull work for added and deleting items from a cart)

--- src/main/java/com/example/transactions/dao/CartDao.java
+++ src/main/java/com/example/transactions/dao/CartDao.java
@@ -2,6 +2,8 @@ package com.example.transactions.dao;
 
 import com.example.catalog.dao.ProductDao;
 import com.example.catalog.model.Product;
+import com.example.transactions.dto.ApiMessage;
+import com.example.transactions.dto.ApiResponse;
 import com.example.transactions.model.Cart;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,25 +19,42 @@ public class CartDao {
     private final JdbcTemplate jdbc;
     private final ProductDao productDao;
 
-
-
     public CartDao(@Qualifier("transactionsJdbcTemplate") JdbcTemplate jdbc, ProductDao pDoa) {
         this.jdbc = jdbc;
         this.productDao = pDoa;
     }
 
-    private RowMapper<CartDao> cartRowMapper = ((rs, rowNum) -> mapCart(rs));
+    //private RowMapper<CartDao> cartRowMapper = (rs, rowNum) -> mapCart(rs);
 
     private Cart mapCart(ResultSet rs) throws SQLException {
         Cart c = new Cart();
         c.setCart_id(rs.getLong("cart_id"));
-        c.setCustomer_id(rs.getLong("customer_id"));
+        c.setUser_id(rs.getLong("customer_id"));
+        c.setProduct_ids((List<Long>) rs.getArray("product_ids"));
+        c.setQuantity(rs.getLong(("quantity")));
         return c;
     }
 
-    public List<Product> findAll (int limit, int offset) {
-        String sql = "SELECT * FROM prodcuts LIMIT ? OFFSET ?";
-        return jdbc.query(sql, cartRowMapper, limit, offset);
+    public ApiResponse addCart(Cart cart) {
+        String sql = "INSERT INTO cart (cart_id, user_id, product_ids, quantity) VALUES (?, ?, ?, ?)";
+        jdbc.update(sql, cart.getCart_id(), cart.getUser_id(), cart.getProduct_ids(), cart.getQuantity());
+        return null;
+    }
+
+    public ApiResponse updateCart(Cart cart) {
+        long temp = cart.getQuantity();
+        String sql = """
+                 UPDATE cart
+                 SET product_ids=?, quantity=?
+                 WHERE cart_id=?
+                 """;
+        jdbc.update(sql, cart.getProduct_ids(), cart.getQuantity());
+        if (cart.getQuantity() > temp) {
+            return new ApiMessage("Item Added to user " + cart.getUser_id() + "'s Cart");
+        } else if (cart.getQuantity() < temp){
+            return new ApiMessage("Item deleted from user " + cart.getUser_id() + "'s Cart");
+        }
+        return new ApiMessage("No change made to user " + cart.getUser_id() + "'s Cart");
     }
 
 }
